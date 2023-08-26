7c081e4df46b5cc59263f1542e322eb768c5b33a
francosorbello <franco.sorbelo@gmail.com>
2023-08-26 01:08:30 -0300

    added script to instantiate objects in the origin of the world

---
+++ Editor/InstanciateInOrigin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+namespace francosorbello.ugmodframework.Editor
+{
+    /// <summary>
+    /// Instantiates a prefab in the origin of the world.
+    /// </summary>
+    public class InstanciateInOrigin : MonoBehaviour
+    {
+        [MenuItem("Assets/Tools/Instantiate Prefab in Origin")]
+        public static void DoInstantiateInOrigin()
+        {
+            var selectedObject = Selection.activeObject;
+            Debug.Log(selectedObject.GetType());
+            
+            if(selectedObject.GetType() == typeof(GameObject))
+            {
+                GameObject instatiatedPrefab =(GameObject) PrefabUtility.InstantiatePrefab(selectedObject as GameObject);
+                instatiatedPrefab.transform.SetPositionAndRotation(Vector3.zero,Quaternion.identity);
+            }
+        }
+    }
+
+}
